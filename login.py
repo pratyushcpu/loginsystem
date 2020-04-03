3df7240484cebcce4ad40d8b50a5cf56decd343c
Pratyush Upreti <59332192+pratyushcpu@users.noreply.github.com>
2020-04-03 14:22:19 +0530

    Create login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+while True:
+	username=input("Please enter the username: ")
+	password=input("Please enter the password: ")
+
+	if (username == "Prat up tech") and (password == "1234"):
+		print("Access Granted")
+		break
+	else:
+		print("Access Denied")
